8fdc9eb518b6f356eaeea96a57a16069860ff776
graz1z.dev <gsreisdejesus@gmail.com>
2024-11-14 21:48:45 -0300

    tentei

--- CuidaMais/Database.java
+++ CuidaMais/Database.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Database {
-    
+
+    private List<Cuidador> cuidadores = new ArrayList<>();
+
+    public List<Cuidador> getCuidadores() {
+        return new ArrayList<>(cuidadores);
+    }
+
+    public void setCuidadores(List<Cuidador> cuidadores) {
+        new ArrayList<>(cuidadores);
+        this.cuidadores = cuidadores;
+    }
+
 }
--- CuidaMais/Principal.java
+++ CuidaMais/Principal.java
@@ -1,48 +1,63 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.List;
+
 
 public class Principal {
     public static void main(String[] args) {
-        ArrayList<Cuidador> arrayCuidadores= new ArrayList<>(); 
 
+        List<Cuidador> cuidadores = new ArrayList<>();
         Scanner entrada = new Scanner(System.in);
         String login;
         String resposta;
+        int i = 0;
 
-        System.out.println("CUIDAMAIS+");
-        System.out.println("Como deseja acessar o sistema?\n[1] Cuidador\n[2] Familiar");
         while (true) {
-            resposta = entrada.nextLine();
-            if (resposta.equals("1") || resposta.equals("2")) {
-                break;
-            } else {
-                System.out.println("Responda corretamente.");
+            System.out.println("CUIDAMAIS+");
+            System.out.println("Como deseja acessar o sistema?\n[1] Cuidador\n[2] Familiar");
+
+            while (true) {
+                resposta = entrada.nextLine();
+                if (resposta.equals("1") || resposta.equals("2")) {
+                    break;
+                } else {
+                    System.out.println("Responda corretamente.");
+                }
             }
-        }
+    
+            if (resposta.equals("1")) {
+                // O USUÁRIO É CUIDADOR
+                System.out.println("Você selecionou a opção Cuidador.");
+                System.out.println("Digite seu CPF para fazer login. Caso queira se cadastrar, digite '000'.");
+                login = entrada.nextLine();
 
-        if (resposta.equals("1")) {
-            // O USUÁRIO É CUIDADOR
-            System.out.println("Você selecionou a opção Cuidador.");
-            System.out.println("Digite seu CPF para fazer login. Caso queira se cadastrar, digite '000'.");
-            login = entrada.nextLine();
-            if (login.equals("000")) {
-                System.out.println("Insira seu CPF");
-                String cpf = entrada.nextLine();
-                System.out.println("Insira seu nome");
-                String nome = entrada.nextLine();
-                System.out.println("Insira sua idade");
-                int idade = entrada.nextInt();
-                System.out.println("Insira seu telefone");
-                String telefone = entrada.nextLine();
+                if (login.equals("000")) {
+                    System.out.println("Insira seu CPF");
+                    String cpf = entrada.nextLine();
+                    System.out.println("Insira seu nome");
+                    String nome = entrada.nextLine();
+                    System.out.println("Insira sua idade");
+                    int idade = entrada.nextInt();
+                    System.out.println("Insira seu telefone");
+                    String telefone = entrada.nextLine(); 
+                    
+                    cuidadores.add(i, null);
+                    i++;
+                    System.out.println("Usuário criado!");
+                }
 
-                Cuidador cuidador = new Cuidador(cpf, nome, idade, telefone);
+                else{
 
-                System.out.println("Usuário criado!");
+                    System.out.println("Listar cadastro [3]");
+                    System.out.println(cuidadores);
+                }
             }
+            if (resposta.equals("2")) {
+                // O USUÁRIO É FAMILIAR
+            }
+
         }
-        if (resposta.equals("2")) {
-            // O USUÁRIO É FAMILIAR
-        }
+        
         
 
 
